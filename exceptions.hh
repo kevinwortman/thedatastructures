469eef1472384e12aaa640b3b93a8ffe5607bf73
Kevin Wortman <kwortman@gmail.com>
2015-09-10 11:29:33 -0700

    import exceptions

---
+++ exceptions.hh
@@ -0,0 +1,31 @@
+
+#pragma once
+
+#include <string>
+
+// Invalid array index: either negative, or greater than or equal to
+// the array length.
+class IndexException {
+public:
+  IndexException(int length, int index)
+    : _length(length),
+      _index(index) { }
+
+  int length() { return _length; }
+  int index()  { return _index;  }
+
+private:
+  int _length, _index;
+};
+
+// Invalid argument passed to a function.
+class ArgumentException {
+public:
+  ArgumentException(std::string& description)
+    : _description(description) { }
+
+  std::string& description() { return _description; }
+
+private:
+  std::string _description;
+};
