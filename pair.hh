7482e4402827891177302e9b4ce3145d74ff098b
Kevin Wortman <kwortman@gmail.com>
2016-02-02 11:05:28 -0800

    fix whitespace

--- pair.hh
+++ pair.hh
@@ -9,10 +9,10 @@ public:
     _right = right;
   }
 
-  LEFT left()   { return _left;  }
+  LEFT left() { return _left;  }
   RIGHT right() { return _right; }
   
-  void set_left(LEFT left)    { _left = left;   }
+  void set_left(LEFT left) { _left = left;   }
   void set_right(RIGHT right) { _right = right; }
   
 private:
